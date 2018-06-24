b9e3467532dab4fa06ac6d1d011e00fbe7bc5c72
Luis Hankel <kaes3kuch3n@gmail.com>
2018-06-24 15:20:43 +0200

    Just a very small optimization :)

--- src/Screen.java
+++ src/Screen.java
@@ -47,11 +47,9 @@ public class Screen extends JPanel {
     }
 
     private Point centerOf(Point p1, Point p2) {
-        Point c = new Point();
         int x = Math.round((float) (p1.x + p2.x) / 2);
         int y = (p1.y + p2.y) / 2;
-        c.setLocation(x, y);
-        return c;
+        return new Point(x, y);
     }
 
     private Color level(int factor) {
